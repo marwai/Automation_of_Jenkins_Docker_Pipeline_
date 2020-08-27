9eaed66ab46d3e4a67201bc99c09f93b00b99529
marwai <mtse@spartaglobal.com>
2020-08-27 19:00:53 +0100

    update work

--- install_jenkins_and_java.sh
+++ install_jenkins_and_java.sh
@@ -2,6 +2,10 @@
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
 
+sudo apt-get update
+
+# sudo apt-get remove jenkins if bugs occurs and install openjdk
+
 sudo apt install openjdk-8-jre
 
 sudo apt-get update
