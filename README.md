935bacd3343f08d9a4126b6f1c6d6fdfc437b1c3
marwai <mtse@spartaglobal.com>
2020-08-27 16:54:44 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Set up instance
+
+# make provision file
+
+# Install the dependencies 
+
+1) download jenkins using recommended settings
+
+2) enter details http://34.243.215.99:8080/
---
+++ install_jenkins_and_java.sh
@@ -0,0 +1,13 @@
+#test
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+
+sudo apt install openjdk-8-jre
+
+sudo apt-get update
+
+sudo apt-get install jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
+
